ee539c30cc423c836f67adc596bc9dbdb9732642
Nataliia Kalynovska <112168059+TaleQ@users.noreply.github.com>
2023-05-17 10:54:01 +0200

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,23 +8,25 @@ import {
 } from './Home.styled';
 import { NavLink } from 'react-router-dom';
 
-const Home = () => (
-  <HomeWrapper>
-    <HomeBox>
-      <HomeTitle>Welcome to Phonebook!</HomeTitle>
-      <HomeText>
-        This is a convenient and user-friendly platform for you to store and
-        access your contacts securely.
-      </HomeText>
-      <HomeText>Discover our main advantages:</HomeText>
-      <div>
-        <HomeSlider />
-      </div>
-      <StartNowBtn type="button">
-        <NavLink to="/contacts">Start now</NavLink>
-      </StartNowBtn>
-    </HomeBox>
-  </HomeWrapper>
-);
+const Home = () => {
+  return (
+    <HomeWrapper>
+      <HomeBox>
+        <HomeTitle>Welcome to Phonebook!</HomeTitle>
+        <HomeText>
+          This is a convenient and user-friendly platform for you to store and
+          access your contacts securely.
+        </HomeText>
+        <HomeText>Discover our main advantages:</HomeText>
+        <div>
+          <HomeSlider />
+        </div>
+        <StartNowBtn type="button">
+          <NavLink to="/contacts">Start now</NavLink>
+        </StartNowBtn>
+      </HomeBox>
+    </HomeWrapper>
+  );
+};
 
 export default Home;
