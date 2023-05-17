21d21b2270ff68ef09ea3d7a619435860ed103e5
Nataliia Kalynovska <112168059+TaleQ@users.noreply.github.com>
2023-05-17 10:47:35 +0200

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,35 +7,24 @@ import {
   StartNowBtn,
 } from './Home.styled';
 import { NavLink } from 'react-router-dom';
-import { useEffect } from 'react';
 
-const Home = () => {
-  useEffect(() => {
-    document.body.classList.add('home-bg');
-
-    return () => {
-      document.body.classList.remove('home-bg');
-    };
-  }, []);
-
-  return (
-    <HomeWrapper>
-      <HomeBox>
-        <HomeTitle>Welcome to Phonebook!</HomeTitle>
-        <HomeText>
-          This is a convenient and user-friendly platform for you to store and
-          access your contacts securely.
-        </HomeText>
-        <HomeText>Discover our main advantages:</HomeText>
-        <div>
-          <HomeSlider />
-        </div>
-        <StartNowBtn type="button">
-          <NavLink to="/contacts">Start now</NavLink>
-        </StartNowBtn>
-      </HomeBox>
-    </HomeWrapper>
-  );
-};
+const Home = () => (
+  <HomeWrapper>
+    <HomeBox>
+      <HomeTitle>Welcome to Phonebook!</HomeTitle>
+      <HomeText>
+        This is a convenient and user-friendly platform for you to store and
+        access your contacts securely.
+      </HomeText>
+      <HomeText>Discover our main advantages:</HomeText>
+      <div>
+        <HomeSlider />
+      </div>
+      <StartNowBtn type="button">
+        <NavLink to="/contacts">Start now</NavLink>
+      </StartNowBtn>
+    </HomeBox>
+  </HomeWrapper>
+);
 
 export default Home;
